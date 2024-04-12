780033854e17cd1e0ccafbda90e05a50407b4e5a
weverton <wevertonmixalt@gmail.com>
2024-04-12 20:19:18 -0300

    a caminho

--- src/app/draw/page.tsx
+++ src/app/draw/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import DebounceInput from "@/components/DebounceInput";
 import DrawCanvas from "@/components/DrawCanvas";
 import { debounce } from "lodash";
 import React, { useCallback, useEffect, useState } from "react";
@@ -23,6 +24,7 @@ const DrawPage: React.FC = () => {
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+      <DebounceInput />
       <div className="w-full h-full flex justify-center items-center gap-4">
         <div>
           <small className="text-white text-left">Drawing</small>
